eeff0f7a0fad47cf6fc2f1898e6f45eb7adaa65a
NadezhdaPavlova2 <127777835+NadezhdaPavlova2@users.noreply.github.com>
2023-09-02 12:21:32 +0300

    Update pages.py

--- Rostelecom/pages.py
+++ Rostelecom/pages.py
@@ -26,36 +26,43 @@ class AuthPage(BasePage):
         self.mail = self.driver.find_element(*AuthLocators.MAIL_AUTH_BTN)
         time.sleep(3)
 
+    #ввод телефона или почты
     def enter_num(self, value):
         self.email.send_keys(value)
 
+    #ввод пароля
     def enter_pass(self, value):
         self.passw.send_keys(value)
 
+    #нажатие кнопки войти
     def btn_click(self):
         self.btn.click()
 
+    #нажатие таб почта
     def emailbtn_click(self):
         self.emailbtn.click()
 
+    #нажатие таб логин
     def loginbtn_click(self):
         self.loginbtn.click()
 
+    #нажатие таб лицевой счет
     def lsbtn_click(self):
         self.lsbtn.click()
 
+    #нажатие кнопка зарегистрироваться
     def reg_btn_click(self):
         self.regbtn.click()
 
+    #нажатие кнопки входа с помощью вк
     def vk_btn_click(self):
         self.vk.click()
 
-    def click_vis_pass(self):
-        self.vispass.click()
-
+    #нажатие кнопки входа с помощью одноклассников
     def ok_btn_click(self):
         self.ok.click()
 
+    #нажатие кнопки входа с помощью mail.ru
     def mail_btn_click(self):
         self.mail.click()
 
@@ -76,12 +83,15 @@ class RegPage(BasePage):
         self.registbtn = self.driver.find_element(*RegLocators.REGIS_BTN)
         time.sleep(3)
 
+    #вводит имя 
     def enter_name(self, value):
         self.name.send_keys(value)
 
+    #вводит фамилию
     def enter_surname(self, value):
         self.surname.send_keys(value)
 
+    #выбор региона
     def click_region(self, value):
         region = self.driver.find_element(By.XPATH, "//input[@autocomplete='new-password']")
         actions = ActionChains(self.driver)
@@ -91,15 +101,19 @@ class RegPage(BasePage):
         actions.pause(5)
         actions.perform()
 
+    #ввод почты
     def enter_mail(self, value):
         self.emailornum.send_keys(value)
 
+    #ввод пароля
     def enter_pass_reg(self, value):
         self.password.send_keys(value)
 
+    #ввод подтверждения пароля
     def enter_confirmpass(self, value):
         self.confirmpass.send_keys(value)
 
+    #нажимает кнопку зарегистрироваться
     def click_reg_btn(self):
         self.registbtn.click()
 
@@ -113,6 +127,7 @@ class ProfilePage(BasePage):
         self.driver.get(url)
         time.sleep(3)
 
+    #проверяет, что кнопка "выйти" присутствует на открывшейся странице, иначе ошибка
     def btn_quit(self):
         try:
             element = self.driver.find_element(*ProfileLocators.QUIT_BTN)
@@ -131,16 +146,20 @@ class VKPage(BasePage):
         self.continuebtn = self.driver.find_element(*VKLocators.CON_BTN)
         time.sleep(3)
 
+    #вводит номер
     def enter_vk_num(self, value):
         self.vknum.send_keys(value)
 
+    #нажимает кнопку "продолжить"
     def continue_btn_click(self):
         self.continuebtn.click()
 
+    #вводит пароль
     def enter_pass_vk(self, value):
         self.passvk = self.driver.find_element(*VKLocators.PASS_VK)
         self.passvk.send_keys(value)
 
+    #нажимает кнопку входа с помощью вк
     def click_vk_auth_btn(self):
         self.vkauthbtn = self.driver.find_element(*VKLocators.VK_AUTH_BTN)
         self.vkauthbtn.click()
@@ -158,12 +177,15 @@ class OKPage(BasePage):
         self.okbtn = self.driver.find_element(*OKLocators.OK_BTN)
         time.sleep(3)
 
+    #вводит номер
     def enter_ok_num(self, value):
         self.oknum.send_keys(value)
 
+    #вводит пароль
     def enter_ok_pass(self, value):
         self.okpass.send_keys(value)
 
+    #нажимает кнопку входа
     def click_ok_btn(self):
         self.okbtn.click()
 
@@ -180,12 +202,15 @@ class MailPage(BasePage):
         self.mailbtn = self.driver.find_element(*MailLocators.MAIL_BTN)
         time.sleep(3)
 
+    #вводит логин
     def enter_mail_login(self, value):
         self.maillogin.send_keys(value)
 
+    #вводит пароль
     def enter_mail_pass(self, value):
         self.mailpass.send_keys(value)
 
+    #нажимает кнопку входа
     def click_mail_btn(self):
         self.mailbtn.click()
 
